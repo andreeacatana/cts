8d8349566d8a20fac500b7345863e00f6398516c
Andreea <Andreea@DESKTOP-G7TG2UV>
2021-03-25 11:19:27 +0200

    prototype

---
+++ sem5/src/ro/ase/cts/prototype/Main.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.prototype;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Reteta reteta1 = new Reteta("Apa oxigenata",50);
+		Reteta reteta2 = (Reteta) reteta1.copiaza();
+		
+		System.out.println(reteta1);
+		System.out.println(reteta2);
+
+	}
+
+}
