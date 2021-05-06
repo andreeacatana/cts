e9e2a1b091475b153e16cb6bfa23cd94980f915a
Andreea <Andreea@DESKTOP-G7TG2UV>
2021-05-06 10:10:08 +0300

    command

---
+++ sem10/src/ro/ase/cts/command/clase/ComandaConstituire.java
@@ -0,0 +1,15 @@
+package ro.ase.cts.command.clase;
+
+public class ComandaConstituire extends Comanda{
+	
+	@Override
+	public void executa() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public ComandaConstituire(ContBancar contBancar, float suma) {
+		super(contBancar, suma);
+	}
+
+}
---
+++ sem10/src/ro/ase/cts/command/clase/ComandaDepunere.java
@@ -0,0 +1,13 @@
+package ro.ase.cts.command.clase;
+
+public class ComandaDepunere extends Comanda{
+	@Override
+	public void executa() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public ComandaDepunere(ContBancar contBancar, float suma) {
+		super(contBancar, suma);
+	}
+}
